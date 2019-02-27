d85c9f0a543008a2555181e2f79c0837b3a35cbb
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-27 18:20:05 +0530

    Update FibonacciNumberCheck.java

--- src/main/java/com/talfinder/assessment/FibonacciNumberCheck.java
+++ src/main/java/com/talfinder/assessment/FibonacciNumberCheck.java
@@ -2,7 +2,18 @@ package com.talfinder.assessment;
 
 public class FibonacciNumberCheck {
 
-  public static boolean isFibonacci(int num) {
-    return false;
-  }
+    public static boolean isFibonacci(int num) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is Fibonacci : " + isFibonacci(9));
+    }
 }
